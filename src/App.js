65e49c0dca7a45cca09b75f93103bcee3fabf36f
robinucar <mehmet.ucar@imediapro.co.uk>
2020-11-08 23:32:26 +0000

    productdata.json

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import productData from './components/product_data.json';
+
+
+const App = () => {
+  console.log(productData)
+return (
+  <SafeAreaView>
+    <View>
+      <Text>
+       
+      </Text>
+    </View>
+  </SafeAreaView>
+)
+}
+
+export default App;
