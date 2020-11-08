9a5e79d4a62a2028c4ae53fe1a7d4c24c987e7c6
robinucar <mehmet.ucar@imediapro.co.uk>
2020-11-08 23:41:10 +0000

    FlatList component added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import {SafeAreaView, View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
 import productData from './components/product_data.json';
 
-
 const App = () => {
-  console.log(productData)
-return (
-  <SafeAreaView>
-    <View>
-      <Text>
-       
-      </Text>
-    </View>
-  </SafeAreaView>
-)
-}
+  console.log(productData);
+  return (
+    <SafeAreaView>
+      <View>
+        <FlatList
+          keyExtractor={(_, index) => index.toString()}
+          data={productData}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default App;
