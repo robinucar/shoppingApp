901f5411ad65f870f7c329b72068b487b661a9cc
robinucar <mehmet.ucar@imediapro.co.uk>
2020-11-09 01:29:07 +0000

    stock info added

--- src/components/ProductCart.js
+++ src/components/ProductCart.js
@@ -4,11 +4,16 @@ import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
 const ProductCart = (props) => {
   const {product} = props;
 
+  const stock = product.inStock === true ? null : <Text>This item is Not in the Stock!</Text>;
+
   return (
     <View style={styles.container}>
       <Image source={{uri: product.imgURL}} style={styles.image} />
       <Text style={{alignSelf: 'center'}}>{product.title}</Text>
-      <Text style={{fontWeight: 'bold', alignSelf: 'center'}}>{product.price}</Text>
+      <Text style={{fontWeight: 'bold', alignSelf: 'center'}}>
+        {product.price}
+      </Text>
+      <Text style={{color: 'red', fontSize: 15, fontWeight: 'bold', alignSelf: 'center'}}>{stock}</Text>
     </View>
   );
 };
@@ -25,6 +30,6 @@ const styles = StyleSheet.create({
   },
   image: {
     height: Dimensions.get('window').height / 4,
-    resizeMode: "contain",
+    resizeMode: 'contain',
   },
 });
