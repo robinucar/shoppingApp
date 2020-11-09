4554bb93d03c3352ae017c698a97cd324e4919b0
robinucar <mehmet.ucar@imediapro.co.uk>
2020-11-09 01:21:23 +0000

    filtered items

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   View,
   Text,
-  TouchableOpacity,
   StyleSheet,
   FlatList,
   Alert,
+  TextInput,
 } from 'react-native';
 import productData from './components/product_data.json';
-import { ProductCart } from './components';
+import {ProductCart} from './components';
 
 const App = () => {
-  const renderListItem = ({item}) => <ProductCart product={item} />
+  const [searchValue, setSearchValue] = useState('');
+  const [displayList, setDisplayList] = useState([]);
+
+  const renderListItem = ({item}) => <ProductCart product={item} />;
 
   useEffect(() => {
-    Alert.alert('RobinShop', 'Welcome to RobinShop...')
+    Alert.alert('RobinShop', 'Welcome to RobinShop...');
+    setDisplayList(productData);
   }, []);
 
-;  return (
+  useEffect(() => {
+    const filteredValue = productData.filter((item) => {
+      const text = searchValue.toUpperCase();
+      const productTitle = item.title.toUpperCase();
+
+      return productTitle.indexOf(text) > -1;
+    });
+    setDisplayList(filteredValue);
+  }, [searchValue]);
+  return (
     <SafeAreaView style={{flex: 1}}>
-      <View>
+      <View style={{flex: 1}}>
         <Text style={styles.header}>ROBINSHOP</Text>
+        <View style={styles.searchBar}>
+          <TextInput
+            placeholder="Search Item..."
+            onChangeText={(value) => setSearchValue(value)}
+          />
+        </View>
         <FlatList
           keyExtractor={(_, index) => index.toString()}
           data={productData}
+          data={displayList}
           renderItem={renderListItem}
         />
       </View>
@@ -39,6 +59,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'purple',
     fontSize: 40,
-    textAlign: 'center'
-  }
-})
+    textAlign: 'center',
+  },
+  searchBar: {
+    backgroundColor: '#eceff1',
+    padding: 8,
+    margin: 5,
+    borderRadius: 10,
+  },
+});
